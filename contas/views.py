4d7574e3e57c6fb753f433a5c6f4f7edb1d7187f
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-02-27 11:36:59 -0300

    add pagina listagem

--- contas/views.py
+++ contas/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+from .models import Transacao
 
 def home(request):
    data = dict(nome='Kaio', idade=37, nacionalidade='brasileiro', frutas=['melancia', 'banana', 'laranja'])
    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+   data = dict(transacoes = Transacao.objects.all)
+   return render(request, 'contas/listagem.html', data)
