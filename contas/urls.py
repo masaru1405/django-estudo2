ccafcdc134ef734cd5b4f5a21743b7fd3f0a905b
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-02-28 15:15:28 -0300

    create e update

--- contas/urls.py
+++ contas/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.home, name='home'),
-   path('listagem/', views.listagem, name='listagem')
+   path('', views.welcome, name='welcome'),
+   path('contas/', views.contas, name='contas'),
+   path('listagem/', views.listagem, name='listagem'),
+   path('nova_transacao/', views.nova_transacao, name='nova_transacao'),
+   path('update/<int:id>/', views.update, name='update'),
 ]
--- contas/views.py
+++ contas/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Transacao
+from .form import TransacaoForm
 
-def home(request):
+def welcome(request):
+   return render(request, 'contas/welcome.html')
+
+def contas(request):
    data = dict(nome='Kaio', idade=37, nacionalidade='brasileiro', frutas=['melancia', 'banana', 'laranja'])
-   return render(request, 'contas/home.html', data)
+   return render(request, 'contas/contas.html', data)
 
 def listagem(request):
    data = dict(transacoes = Transacao.objects.all)
    return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+   data = dict(form=TransacaoForm(request.POST or None))
+   if data['form'].is_valid():
+      data['form'].save()
+      return redirect('listagem') #volta para a página listagem
+   return render(request, 'contas/form.html', data)
+
+def update(request, id):
+   transacao = Transacao.objects.get(pk=id)
+   data = dict(form=TransacaoForm(request.POST or None, instance=transacao))
+   if data['form'].is_valid():
+      data['form'].save()
+      return redirect('listagem')
+   return render(request, 'contas/form.html', data)
